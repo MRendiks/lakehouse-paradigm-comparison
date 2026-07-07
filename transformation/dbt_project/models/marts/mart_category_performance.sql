aee8e763599226dc278e0254c2d650be68d3c11e
M. Rendi <rendi@jkt1.ebdesk.com>
2026-07-07 16:06:40 +0700

    :sparkles: refactor: simplify mart_category_performance SQL by removing unnecessary CTEs and improving readability

--- transformation/dbt_project/models/marts/mart_category_performance.sql
+++ transformation/dbt_project/models/marts/mart_category_performance.sql
@@ -1,17 +1,16 @@
-with order_items as (
-    select * from {{ ref('fct_order_items') }}
-),
-products as (
-    select * from {{ ref('dim_products') }}
-)
+{{
+  config(
+    materialized='table'
+  )
+}}
 
 select
     p.product_category_name,
     count(distinct oi.order_id) as total_orders,
-    count(oi.order_item_id) as total_items_sold,
-    sum(oi.price) as total_revenue,
+    sum(oi.order_item_id) as total_items_sold,
+    sum(oi.total_item_value) as total_revenue,
     avg(oi.price) as average_item_price
-from products p
-left join order_items oi on p.product_id = oi.product_id
-where p.product_category_name is not null
-group by 1
+from {{ ref('fct_order_items') }} oi
+join {{ ref('dim_products') }} p
+    on oi.product_id = p.product_id
+group by p.product_category_name
